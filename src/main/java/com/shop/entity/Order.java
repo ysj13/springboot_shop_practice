b7f8f9b505471122ede5b3db88af64e35ad01e43
ysj <ysj13@kakao.com>
2025-01-03 02:21:04 +0900

    Auditing 을 이용한 Entity 속성 공통화

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
-public class Order {
+public class Order extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -31,8 +31,4 @@ public class Order {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
-
-    private LocalDateTime regTime;
-
-    private LocalDateTime updateTime;
 }
--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -28,8 +28,4 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
-
-    private LocalDate regTime;
-
-    private LocalDateTime updateTime;
 }
